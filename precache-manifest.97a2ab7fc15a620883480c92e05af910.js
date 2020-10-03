f3a5738ebedd728bd7ddc25ece2ebc4d634178e4
Oksiuk <yelyzaveta.oksiuk@gmail.com>
2020-10-03 21:53:06 +0300

    Updates

---
+++ precache-manifest.97a2ab7fc15a620883480c92e05af910.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd4ca8f132c1c90ae356f6e7cdfe33ce",
+    "url": "/recipe-box-app/index.html"
+  },
+  {
+    "revision": "17c03fc6a65ee4f1e81c",
+    "url": "/recipe-box-app/static/css/main.fa426e26.chunk.css"
+  },
+  {
+    "revision": "20a7965c5c2718c65126",
+    "url": "/recipe-box-app/static/js/2.1ed74b75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-box-app/static/js/2.1ed74b75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17c03fc6a65ee4f1e81c",
+    "url": "/recipe-box-app/static/js/main.6f8d9893.chunk.js"
+  },
+  {
+    "revision": "37f2936404f0cc5aa0f5",
+    "url": "/recipe-box-app/static/js/runtime-main.85ea07c2.js"
+  },
+  {
+    "revision": "5f098c8f186ea292360f86eaeef8aeea",
+    "url": "/recipe-box-app/static/media/bg.5f098c8f.svg"
+  },
+  {
+    "revision": "5e4a6b75ede6d4a5becb91821e57853c",
+    "url": "/recipe-box-app/static/media/clear.5e4a6b75.svg"
+  },
+  {
+    "revision": "eea18703d306b9c8ec78595ecf673e27",
+    "url": "/recipe-box-app/static/media/edit.eea18703.svg"
+  },
+  {
+    "revision": "705a940e6b8b38ecc66bc1ebe94a6dc5",
+    "url": "/recipe-box-app/static/media/plus.705a940e.svg"
+  },
+  {
+    "revision": "1606bdc601491aa380fbc8b1e6a4786d",
+    "url": "/recipe-box-app/static/media/sriracha-v4-latin-regular.1606bdc6.woff"
+  },
+  {
+    "revision": "2178a5eae45917aa9feb5d372aa80640",
+    "url": "/recipe-box-app/static/media/sriracha-v4-latin-regular.2178a5ea.ttf"
+  },
+  {
+    "revision": "71fe11d2e6c064467864166285069849",
+    "url": "/recipe-box-app/static/media/sriracha-v4-latin-regular.71fe11d2.svg"
+  },
+  {
+    "revision": "80eaa80ad12a7ad43a00d9259d15a262",
+    "url": "/recipe-box-app/static/media/sriracha-v4-latin-regular.80eaa80a.eot"
+  },
+  {
+    "revision": "a85d252783d2d310c30d15044f23c778",
+    "url": "/recipe-box-app/static/media/sriracha-v4-latin-regular.a85d2527.woff2"
+  }
+]);
